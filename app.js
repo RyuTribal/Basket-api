58a6ca492ed740804e96315c15eb5a146ed05984
Ivan Sedelkin <ivan.sedelkin9@gmail.com>
2019-07-28 18:01:45 +0200

    fixed port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-//var api = require('./routes/group');
+var api = require('./routes/group');
 var mongoose = require('mongoose');
 var PORT = process.env.PORT || 4242;
 var host = process.env.YOUR_HOST || '0.0.0.0';
@@ -22,14 +22,14 @@ app.use(function(req, res, next) {
     next();
 });
 
-// app.use(bodyParser.json({limit: '50mb'}));
-// app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-
 app.use(express.static(__dirname + '/www'));
 
 app.use('/', home);
 
-//app.use('/api/1.0', api);
+app.use('/api/1.0', api);
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 app.listen(PORT, host, function(){
     console.log('Server up and running');
